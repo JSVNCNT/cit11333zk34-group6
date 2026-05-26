fd43ff26c86ca7fc27c62ff8784bcdd36b3d4fe5
Jose Vincent Bayoguing <joseiiibayoguing@gmail.com>
2026-05-27 01:59:43 +0800

    Deployed project to InfinityFree hosting

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
 
 $conn = mysqli_connect(
-    "localhost",
-    "root",
-    "",
-    "student_system"
+    "sql113.infinityfree.com",
+    "if0_42026411",
+    "6QmydeWQ35",
+    "if0_42026411_student_system"
 );
 
 if (!$conn) {
